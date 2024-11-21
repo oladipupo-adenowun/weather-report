0c5b1f114024599d37952b21750aec7d7f7788ca
Oladipupo Adenowun <oladipupo.adenowun@yahoo.com>
2024-11-20 22:55:34 -0500

    added the weather features

---
+++ script.js
@@ -0,0 +1,26 @@
+function showWeatherDetails(event){
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    const apiKey = 'feb4f7810621947469de6c6f12719d95';
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric';
+
+    //https://api.openweathermap.org/data/2.5/weather?q=Brooklyn&appid=feb4f7810621947469de6c6f12719d95&units=metric
+
+    console.log(apiUrl);
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `
+                                        <h2>Weather in ${data.name}</h2>
+                                        <p>Temperature: ${data.main.temp} &#8451;</p>
+                                        <p>Weather: ${data.weather[0].description}
+                                    `;
+        })
+        .catch(error =>{
+            console.log(error);
+        });
+
+    document.getElementById("weatherForm").addEventListener('submit',showWeatherDetails);
+}
